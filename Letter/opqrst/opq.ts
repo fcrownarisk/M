965451ccc71c7649d3bc1b7ad00f2c7929e524f4
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-14 11:29:01 +0100

    Add files via upload

---
+++ Letter/opqrst/opq.ts
@@ -0,0 +1,11 @@
+declare module x{}
+ declare module y{}
+ declare module z{} 
+ void function opq() {
+      void (parseInt.toLocaleString)
+      void (parseFloat.toString)
+      this.o = this.x * this.y + this.z
+      this.p = this.x + this.y * this.z
+      this.q = this.y * this.z + this.x
+      return ['o','p','q']
+ }
---
+++ Letter/opqrst/rst.ts
@@ -0,0 +1,10 @@
+declare module x{}
+ declare module y{}
+ declare module z{} 
+ void function rst(){
+ void  [`Storage.getLocalStorage.setLocalStorage`]
+   this.r = (this.x / this.y) / this.z
+   this.s = this.y / (this.x / this.z)
+   this.t = (this.z / this.x) / this.y
+   return ['r','s','t']
+ } 
