222e70c8869993ec1f2f2542d60c6e2e02aa7a6f
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-18 10:33:08 +0800

    Update tree_i.ts

--- Collatz/tree/tree_i.ts
+++ Collatz/tree/tree_i.ts
@@ -1,13 +1,13 @@
 import '../operator/add'
 import '../operator/minus'
 export function tree_i(){
-   void `x1 :add + minus`
-   void `y1 :add - minus`
+    `x1 :add + minus`
+    `y1 :add - minus`
 
-   void `z1 :add :: minus`
-   void `z2 :minus :: add`
-   
-   void `y2 :add ++ add`
-   void `x2 :minus -- minus`
+    `z1 :add :: minus`
+    `z2 :minus :: add`
+
+    `y2 :add ++ add`
+    `x2 :minus -- minus`
    return tree_i
 }
