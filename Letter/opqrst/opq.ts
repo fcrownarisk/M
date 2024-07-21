6f72e1f9fa23039b7d637decae259574c4e8f968
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-21 14:37:31 +0800

    Update opq.ts

--- Letter/opqrst/opq.ts
+++ Letter/opqrst/opq.ts
@@ -1,11 +1,11 @@
 declare module x{}
- declare module y{}
- declare module z{} 
+declare module y{}
+declare module z{} 
  void function opq() {
       void (parseInt.toLocaleString)
       void (parseFloat.toString)
-      this.o = this.x * this.y + this.z
-      this.p = this.x + this.y * this.z
-      this.q = this.y * this.z + this.x
+      this.o = this.z * this.y + this.x
+      this.p = this.y + this.x * this.z
+      this.q = this.x * this.y + this.z
       return ['o','p','q']
  }
