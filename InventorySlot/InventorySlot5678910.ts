4e12740602554afd50da399798fc4e41366be101
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-09-26 10:34:55 +0100

    Update InventorySlot5678910.ts

--- InventorySlot/InventorySlot5678910.ts
+++ InventorySlot/InventorySlot5678910.ts
@@ -4,10 +4,6 @@ import './XYZT/t6z6y6x6'
 import './return/returnn'
 import './return/returnnn'
 import './return/returnnnn'
-import './Bigletter/A'
-import './Bigletter/B'
-import './Bigletter/C'
-import './Bigletter/D'
 void function InventorySlot5(){
     return [
         {axis: 'x', items: '--- --- ---'},
@@ -32,28 +28,5 @@ void function InventorySlot7(){
         {axis:'t5',items:'^ ^ ^  ^ ^ ^  ^ ^ ^ '}
     ]
 }
-void function InventorySlot8() {
-    return [
-        {axis: 'x6', items: 'AABBCCDD'},
-        {axis: 'y6', items: 'DDCCBBAA'},
-        {axis: 'z6', items: 'ABABCCDD'},
-        {axis: 't6', items: 'ABCDDCBA'},
-    ]
-}
-void function InventorySlot9() {
-    return[
-        {axis: 't1', items: 'ABABCDCD'},
-        {axis: 't2', items: 'DCDCBABA'},
-        {axis: 't3', items: 'ABCD&&DCBA'},
-        {axis: 't4', items: 'ABCD||DCBA'},
-    ]
-}
-void function InventorySlot10(){
-   return [
-        { axis: 't6', items: 'ABAB||DCDC' },
-        { axis: 't7', items: 'ABAB\\//CDCD' },
-        { axis: 't8', items: 'AB\|BA||CD|/DC' },
-        { axis: 't9', items: 'CD&&DC\\//AB&&BA' }
-    ]
-}
+
 
