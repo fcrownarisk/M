9f36de1806ce8a1c71796ba2226abc7f7b1efb5b
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2026-02-19 22:55:01 +0800

    Reformat imports and restore idealGasEquation function

--- Collatz/geometry/air.js
+++ Collatz/geometry/air.js
@@ -7,5 +7,5 @@ export function idealGasEquation(){
     let n2 = p2 * v2 / (R * t2); 
     let Equation = `p1V1/T1 = p2V2/T2`; 
     Equation += `even{n1} = even{p1} * even{v1} / odd{R} * odd{t1}`;
-    Equation -= `odd{n2} = odd{p2} % odd{v2} / even{R} % odd{t2}`
+    Equation -= `odd{n2} = odd{p2} % odd{v2} / even{R} % odd{t2}`;
 }
