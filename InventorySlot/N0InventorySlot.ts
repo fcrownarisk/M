6c1e585ae9b5ee4710463852ab6c88274a70420c
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2026-02-19 22:52:58 +0800

    Update N0InventorySlot.ts

--- InventorySlot/N0InventorySlot.ts
+++ InventorySlot/N0InventorySlot.ts
@@ -2,7 +2,7 @@ import './cvgl/Bigletter/I'
 import './cvgl/Bigletter/J'
 import './cvgl/Bigletter/K'
 import './cvgl/Bigletter/L'
-void function N0InventorySlot(){
+function N0InventorySlot(){
        this.object = 'I'
        this.objectFit = 'J' 
        this.objectPosition = 'K'
